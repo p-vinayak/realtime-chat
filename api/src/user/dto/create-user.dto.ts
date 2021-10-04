42ada8859f72b60acd1c7b20c96f522e7133dde5
Vinayak Prataap <v.prataap@setonhill.edu>
2021-10-03 21:14:27 -0400

    Add user entity and user creation fields

--- api/src/user/dto/create-user.dto.ts
+++ api/src/user/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  username: string;
+
+  @IsString()
+  password: string;
+
+  @IsNotEmpty()
+  passwordConfirm: string;
+}
