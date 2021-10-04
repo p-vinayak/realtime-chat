5ce444a6b22a129fdb2b775b189378dee2c32610
Vinayak Prataap <v.prataap@setonhill.edu>
2021-10-03 21:51:13 -0400

    Add typeorm package

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getConnectionOptions } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: async () =>
+        Object.assign(await getConnectionOptions(), { autoLoadEntities: true }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
