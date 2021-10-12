04a3eb1bdd87139f1dcd2ea806c042440a3bc208
Vinayak Prataap <v.prataap@setonhill.edu>
2021-10-12 12:35:02 -0400

    Use config service to fetch session secret

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as session from 'express-session';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
   app.use(
     session({
-      secret: process.env.SESSION_SECRET,
+      secret: configService.get('SESSION_SECRET'),
       resave: false,
       saveUninitialized: false,
     }),
