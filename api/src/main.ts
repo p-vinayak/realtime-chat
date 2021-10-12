4963117fe8be33ad76ccb7029969c7ddc747649e
Vinayak Prataap <v.prataap@setonhill.edu>
2021-10-12 19:33:51 -0400

    Add redis for session storage

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,12 +3,20 @@ import { AppModule } from './app.module';
 import * as session from 'express-session';
 import { ConfigService } from '@nestjs/config';
 import * as passport from 'passport';
+import { createClient } from 'redis';
+import * as createRedisStore from 'connect-redis';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  const RedisStore = createRedisStore(session);
+  const redisClient = createClient({
+    host: configService.get('REDIS_HOST'),
+    port: configService.get('REDIS_PORT'),
+  });
   app.use(
     session({
+      store: new RedisStore({ client: redisClient }),
       secret: configService.get('SESSION_SECRET'),
       resave: false,
       saveUninitialized: false,
